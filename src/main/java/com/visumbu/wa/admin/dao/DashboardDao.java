7ec194aa0b85d4923dee4e87a84ca1b99744782e
Varghees Samraj <varghees@gmail.com>
2016-10-19 15:36:12 +0530

    Added Dashboard Entry for total visits

--- src/main/java/com/visumbu/wa/admin/dao/DashboardDao.java
+++ src/main/java/com/visumbu/wa/admin/dao/DashboardDao.java
@@ -12,6 +12,8 @@ import java.util.Date;
 import java.util.List;
 import org.hibernate.Query;
 import org.hibernate.transform.Transformers;
+import org.hibernate.type.IntegerType;
+import org.hibernate.type.StringType;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -24,8 +26,14 @@ import org.springframework.transaction.annotation.Transactional;
 public class DashboardDao extends BaseDao {
 
     public List getTopDealersByVisit(Date startDate, Date endDate) {
-        String queryStr = "select dealer.dealer_name dealerName, count(distinct(session_id)) totalSiteVisit, count(1) totalPageVisit, count(distinct(fingerprint)) uniqueUserCount from visit_log, dealer where visit_log.site_id = dealer.id group by 1";
-        Query query = sessionFactory.getCurrentSession().createSQLQuery(queryStr).setResultTransformer(Transformers.aliasToBean(DealerVisitBean.class));
+        String queryStr = "select dealer.dealer_name dealerName, dealer.website website, count(distinct(session_id)) totalSiteVisit, count(1) totalPageVisit, count(distinct(fingerprint)) uniqueUserCount from visit_log, dealer where visit_log.site_id = dealer.id group by 1";
+        Query query = sessionFactory.getCurrentSession().createSQLQuery(queryStr)
+                .addScalar("dealerName", StringType.INSTANCE)
+                .addScalar("website", StringType.INSTANCE)
+                .addScalar("totalSiteVisit", IntegerType.INSTANCE)
+                .addScalar("totalPageVisit", IntegerType.INSTANCE)
+                .addScalar("uniqueUserCount", IntegerType.INSTANCE)
+                .setResultTransformer(Transformers.aliasToBean(DealerVisitBean.class));
         return query.list();
     }
 
