b6e75427d2355a344669facd93efdc81ea522582
netphenix <netphenix@netphenix>
2016-11-13 13:37:22 +0530

    changed index.html

--- src/main/webapp/static/scripts/controllers/app.admin.dealer.js
+++ src/main/webapp/static/scripts/controllers/app.admin.dealer.js
@@ -2,22 +2,40 @@
     'use strict';
     angular.module('app.admin.dealer', ['angularUtils.directives.dirPagination'])
             .controller('DealerController', ['$scope', '$http', '$stateParams', function ($scope, $http, $stateParams) {
-                    $scope.currentPage = 1;
-                    $scope.pageSize = 10;
-                    $scope.reports = [];
-                    console.log("Dealer : " + $stateParams.searchId)
-                    if (!$stateParams.searchId) {
-                        $stateParams.searchId = 0;
-                    }
+//                    $scope.currentPage = 1;
+//                    $scope.pageSize = 10;
+//                    $scope.reports = [];
+//                    console.log("Dealer : " + $stateParams.searchId)
+//                    if (!$stateParams.searchId) {
+//                        $stateParams.searchId = 0;
+//                    }
+                    
+                    
+                    $scope.count = 50;
+                    $scope.total_count = 0;
+                    $scope.num = 1;
+                    var data = {count: $scope.count, page: $scope.page ? $scope.page : 1};
+                    $scope.pageChangeHandler = function (num) {
+                        data.count = 50;
+                        data.page = num;
+                        $http({method: 'GET', url: '../admin/dealer/' + $stateParams.searchId, params: data}).success(function (response) {
+                            $scope.dealers = response.data;
+                            $scope.total_count = response.count;
+                            console.log("Data : "+$scope.visitReports)
+                            console.log("Count : "+$scope.total_count)
+                        });
+                        console.log('reports page changed to ' + num);
+                    };
+                    $scope.pageChangeHandler($scope.num);
 
                     //Dir Pagination       
                     $scope.pageChangeHandler = function (num) {
                         console.log('reports page changed to ' + num);
                     };
 
-                    $http.get("../admin/dealer").success(function (response) {
-                        $scope.dealers = response;
-                    });
+//                    $http.get("../admin/dealer").success(function (response) {
+//                        $scope.dealers = response;
+//                    });
 
                     /*Header Sortable*/
                     $scope.sort = {
