5ba5f14a8e57e667aa3fe7c567454c113b06bec5
duc-dev-04 <duc-dev-04@duc-dev-04>
2016-11-14 11:39:12 +0530

    Dashboard Tabs

--- src/main/webapp/static/scripts/controllers/app.dashboard.js
+++ src/main/webapp/static/scripts/controllers/app.dashboard.js
@@ -8,9 +8,6 @@
                     $scope.totalPageVisitCharts = [];
                     $scope.totalSiteVisitCharts = [];
                     $scope.uniqueUserCountCharts = [];
-                    $scope.firstReferrers = []
-                    $scope.lastReferrers = []
-                    $scope.assistReferrers = []
                     $scope.getItems = function () {
                         if (!$stateParams.searchId) {
                             $stateParams.searchId = 0;
@@ -64,45 +61,7 @@
                             }
                         });
 
-                        $http.get("../admin/report/extremeReferrerSummary/" + $stateParams.searchId + "?" + "startDate=" + $stateParams.startDate + "&" + "endDate=" + $stateParams.endDate).success(function (response) {
-                            if (response.firstReferrer.length === 0) {
-                                $scope.firstReferrerEmptyMessage = true
-                                $scope.firstReferrerErrorMessage = "No Data Found";
-                            } else {
-                                angular.forEach(response.firstReferrer.slice(0, 5), function (value, key) {
-                                    $scope.firstReferrers.push(value);
-                                    console.log($scope.firstReferrers.referrer)
-                                });
-                            }
-
-                            //Last Referrer
-
-                            if (response.lastReferrer.length === 0) {
-                                $scope.lastReferrerEmptyMessage = true
-                                $scope.lastReferrerErrorMessage = "No Data Found";
-                            } else {
-                                angular.forEach(response.lastReferrer.slice(0, 5), function (value, key) {
-                                    $scope.lastReferrers.push(value);
-                                    console.log($scope.lastReferrers.referrer)
-                                });
-                            }
-
-                        });
-
-                        $http.get("../admin/report/referrerAssistSummary/" + $stateParams.searchId + "?" + "startDate=" + $stateParams.startDate + "&" + "endDate=" + $stateParams.endDate).success(function (response) {
-
-                            if (response.assistReferrer.length === 0) {
-                                $scope.assistReferrerEmptyMessage = true
-                                $scope.assistReferrerErrorMessage = "No Data Found";
-                            } else {
-                                angular.forEach(response.assistReferrer.slice(0, 5), function (value, key) {
-                                    $scope.assistReferrers.push(value);
-                                    console.log($scope.lastReferrers.referrer)
-                                });
-                            }
-                        });
-
-
+                        
 
                         $http.get("../admin/report/byFrequency/" + $stateParams.searchId + "?" + "startDate=" + $stateParams.startDate + "&" + "endDate=" + $stateParams.endDate).success(function (response) {
 //                            $scope.frequencies = response.slice(0, 5);
