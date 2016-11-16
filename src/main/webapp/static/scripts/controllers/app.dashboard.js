80897dc738ce36569c5de500bd1faf883bd6b255
duc-dev-04 <duc-dev-04@duc-dev-04>
2016-11-16 18:55:32 +0530

    Fixed stateParams issues

--- src/main/webapp/static/scripts/controllers/app.dashboard.js
+++ src/main/webapp/static/scripts/controllers/app.dashboard.js
@@ -5,6 +5,9 @@
                 function ($scope, $location, toaster, $http, $stateParams) {
                     $scope.path = $stateParams.searchId;
                     
+                    $scope.startDate = $stateParams.startDate;
+                    $scope.endDate = $stateParams.endDate;
+                    
                     $('.nav-tabs li').click(function (e) {
                         $(this).siblings().removeClass('active')
                         $(this).addClass('active')
