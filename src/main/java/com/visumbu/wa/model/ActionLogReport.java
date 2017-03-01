da09b6f6bc52ad7d68ba3ea38bd27e1eeaa4f948
Varghees Samraj <varghees@gmail.com>
2017-03-01 16:31:32 +0530

    Removed processing before saving data in Cookie product

--- src/main/java/com/visumbu/wa/model/ActionLogReport.java
+++ src/main/java/com/visumbu/wa/model/ActionLogReport.java
@@ -47,7 +47,6 @@ public class ActionLogReport implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Basic(optional = false)
     @Column(name = "id")
     private Integer id;
